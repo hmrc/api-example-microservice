366cd3f465a2864752de3689c4ed12cf0632eb87
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2018-06-25 15:52:29 +0100

    API-3262: moving api-example-microservice to the Public GitHub

---
+++ app/uk/gov/hmrc/hello/controllers/HeaderValidator.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hello.controllers
+
+import play.api.mvc._
+
+import scala.concurrent.Future
+import scala.util.matching.Regex
+import scala.util.matching.Regex.Match
+
+
+trait HeaderValidator extends Results with HmrcMimeTypes with ErrorConversion {
+  val validateVersion: String => Boolean = v => v == "1.0" || v == "2.0"
+
+  val validateContentType: String => Boolean = ct => ct == "json" || ct == "xml"
+
+  val matchHeader: String => Option[Match] = new Regex( """^application/vnd[.]{1}hmrc[.]{1}(.*?)[+]{1}(.*)$""", "version", "contenttype") findFirstMatchIn _
+
+  val acceptHeaderValidationRules: Option[String] => Boolean =
+    _ flatMap (a => matchHeader(a) map (res => validateContentType(res.group("contenttype")) && validateVersion(res.group("version")))) getOrElse false
+
+  def validateAction(rules: Option[String] => Boolean) = new ActionBuilder[Request] with ActionFilter[Request] {
+
+    def filter[T](input: Request[T]) = Future.successful {
+
+      implicit val r = input
+
+      if (! rules(input.headers.get("Accept")) ) {
+        Some(ErrorAcceptHeaderInvalid)
+      } else {
+        None
+      }
+    }
+
+  }
+
+  val ValidateAcceptHeader : ActionBuilder[Request] = validateAction(acceptHeaderValidationRules)
+
+}
---
+++ test/unit/uk/gov/hmrc/hello/controllers/HeaderValidatorSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.hello.controllers
+
+import org.scalatest.Matchers
+import uk.gov.hmrc.hello.controllers.HeaderValidator
+import uk.gov.hmrc.play.test.UnitSpec
+
+class HeaderValidatorSpec extends UnitSpec with Matchers with HeaderValidator{
+
+  "acceptHeaderValidationRules" should {
+
+    "return false when the header value is missing" in {
+      acceptHeaderValidationRules(None) shouldBe false
+    }
+
+    "return true when the version and the content type in header value is well formatted" in {
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.1.0+json")) shouldBe true
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.2.0+json")) shouldBe true
+    }
+
+    "return true when the version and the content type in header value is well formatted xml" in {
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.1.0+xml")) shouldBe true
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.2.0+xml")) shouldBe true
+    }
+
+    "return false when the content type in header value is missing" in {
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.1.0")) shouldBe false
+    }
+
+    "return false when the content type in header value is not well formatted" in {
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.v1+json")) shouldBe false
+    }
+
+    "return false when the content type in header value is not valid" in {
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.notvalid+XML")) shouldBe false
+    }
+
+    "return false when the version in header value is not valid" in {
+      acceptHeaderValidationRules(Some("application/vnd.hmrc.notvalid+json")) shouldBe false
+    }
+
+  }
+
+}
+
