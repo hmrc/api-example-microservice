eace9b6d14d632f134a927ef36086ed4c1aed71b
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2022-12-16 14:25:02 +0000

    API-5586: play and library updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,14 @@ import play.core.PlayVersion
 import sbt._
 
 object AppDependencies {
+  val bootStrapVersion = "7.12.0"
+
   lazy val dependencies = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"      % "5.13.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"      % bootStrapVersion
   )
 
   def testDependencies(scope: String) = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"         % "5.13.0"            % scope,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"         % bootStrapVersion            % scope,
     "com.vladsch.flexmark"    %  "flexmark-all"                   % "0.36.8"            % scope,
     "org.scalaj"              %% "scalaj-http"                    % "2.4.0"             % scope,
     "com.github.tomakehurst"  %  "wiremock-jre8-standalone"       % "2.27.1"            % scope,
